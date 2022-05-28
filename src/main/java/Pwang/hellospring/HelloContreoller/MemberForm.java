3a53739a048a8a1a5e08a1fb0bb1fc4f5dcde1cc
bbss456 <69797461+bbss456@users.noreply.github.com>
2022-05-28 13:20:20 +0900

    MVC 패턴 추가

---
+++ src/main/java/Pwang/hellospring/HelloContreoller/MemberForm.java
@@ -0,0 +1,13 @@
+package Pwang.hellospring.HelloContreoller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
