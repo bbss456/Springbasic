bf3b15876ff29a31d570c493227752edb3e25a96
bbss456 <69797461+bbss456@users.noreply.github.com>
2022-06-02 01:11:56 +0900

    AOP 추가 내용

--- src/main/java/Pwang/hellospring/aop/TimeTraceAop.java
+++ src/main/java/Pwang/hellospring/aop/TimeTraceAop.java
@@ -20,7 +20,6 @@ public class TimeTraceAop {
            long finish = System.currentTimeMillis();
            long timMs = finish - start;
            System.out.println("END : "+ joinPoint.toLongString() + " " +timMs + "Ms");
-
        }
     }
 }
